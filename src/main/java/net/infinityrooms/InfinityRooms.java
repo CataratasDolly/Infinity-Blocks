bdcfac59f7924376b8d75612bb4f5637cd2ec2a3
CataratasDolly <bernardobf2010@hotmail.com>
2021-08-20 16:18:57 -0300

    No outline for real

--- src/main/java/net/infinityrooms/InfinityRooms.java
+++ src/main/java/net/infinityrooms/InfinityRooms.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderContext;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
---
+++ src/main/java/net/infinityrooms/mixin/NoOutline.java
@@ -0,0 +1,43 @@
+package net.infinityrooms.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.infinityrooms.InfinityRooms.*;
+
+@Mixin(GameRenderer.class)
+public class NoOutline {
+
+    @Final
+    @Shadow
+    private MinecraftClient client;
+
+    @Inject(method = "shouldRenderBlockOutline", at = @At(value = "HEAD", target = "Lnet/minecraft/client/render/GameRenderer;shouldRenderBlockOutline()Z"), cancellable = true)
+    public void shouldRenderBlockOutline(CallbackInfoReturnable<Boolean> cir) {
+        HitResult hitResult = this.client.crosshairTarget;
+        assert hitResult != null;
+        BlockPos blockPos = ((BlockHitResult)hitResult).getBlockPos();
+        assert this.client.world != null;
+        BlockState blockState = this.client.world.getBlockState(blockPos);
+        Block lookingAt = blockState.getBlock();
+
+        if (lookingAt == InfinityWhite || lookingAt == InfinityOrange || lookingAt == InfinityMagenta || lookingAt == InfinityLightBlue ||
+                lookingAt == InfinityYellow || lookingAt == InfinityLime || lookingAt == InfinityPink || lookingAt == InfinityGray ||
+                lookingAt == InfinityLightGray || lookingAt == InfinityCyan || lookingAt == InfinityPurple || lookingAt == InfinityBlue ||
+                lookingAt == InfinityBrown || lookingAt == InfinityGreen || lookingAt == InfinityRed || lookingAt == InfinityBlack ||
+                lookingAt == InfinityStars) {
+            cir.setReturnValue(false);
+        }
+    }
+}
